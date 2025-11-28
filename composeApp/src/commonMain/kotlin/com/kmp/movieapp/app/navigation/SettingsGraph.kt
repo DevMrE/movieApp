8fa6c555b3dec2965d26970b0f5b79b33de6ac56
MrE <it.emrah.cicek@gmail.com>
2025-11-29 00:15:45 +0100

    code formatting

--- composeApp/src/commonMain/kotlin/com/kmp/movieapp/app/navigation/SettingsGraph.kt
+++ composeApp/src/commonMain/kotlin/com/kmp/movieapp/app/navigation/SettingsGraph.kt
@@ -3,6 +3,7 @@ package com.kmp.movieapp.app.navigation
 import com.kmp.movieapp.settings.SettingsScreenComponent
 import com.kmp.movieapp.settings.destination.SettingsScreenDestination
 import com.kmp.navigation.TypedGraphBuilder
+
 fun TypedGraphBuilder.settingsGraph() {
     screen<SettingsScreenDestination> {
         SettingsScreenComponent()
