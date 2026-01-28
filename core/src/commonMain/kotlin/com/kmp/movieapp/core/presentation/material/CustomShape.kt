eb3a6c73d225e072123a6a9da93260096e0c0a08
MrE <it.emrah.cicek@gmail.com>
2026-01-28 21:59:03 +0100

    remove not needed fun

--- core/src/commonMain/kotlin/com/kmp/movieapp/core/presentation/material/CustomShape.kt
+++
@@ -1,9 +0,0 @@
-package com.kmp.movieapp.core.presentation.material
-
-import androidx.compose.foundation.shape.CornerBasedShape
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.material3.ShapeDefaults
-import androidx.compose.material3.Shapes
-import androidx.compose.ui.unit.dp
-
-fun Shapes.ultraLarge(): CornerBasedShape = ShapeDefaults.ExtraLarge.copy(CornerSize(42.dp))
