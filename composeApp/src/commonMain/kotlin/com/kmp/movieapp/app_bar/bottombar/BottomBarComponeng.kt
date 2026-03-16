04e84de865e0b55ac7c59b8bc654b1e139b048e0
Emrah <it.emrah.cicek@gmail.com>
2026-03-16 12:27:20 +0100

    fix: raw not clickable anymore

--- composeApp/src/commonMain/kotlin/com/kmp/movieapp/app_bar/bottombar/BottomBarComponeng.kt
+++ composeApp/src/commonMain/kotlin/com/kmp/movieapp/app_bar/bottombar/BottomBarComponeng.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
@@ -87,6 +88,7 @@ internal fun BottomBarComponent() {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
+                .clickable(false, onClick = {})
                 .padding(horizontal = MaterialTheme.padding.sixteen)
                 .navigationBarsPadding()
                 .border(
