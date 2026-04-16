5bf803227c0ba6fba470e160a0a8512f3d9d8efa
Emrah <it.emrah.cicek@gmail.com>
2026-04-16 23:11:22 +0200

    adding documentation for LocaleLanguageProvider.kt

--- core/src/commonMain/kotlin/com/kmp/movieapp/core/language/LocaleLanguageProvider.kt
+++ core/src/commonMain/kotlin/com/kmp/movieapp/core/language/LocaleLanguageProvider.kt
@@ -1,6 +1,11 @@
 package com.kmp.movieapp.core.language
 
+/**
+ * Provides the current language of the application.
+ *
+ * This method returns the language that is currently active and should be used,
+ * for example, in API requests or content localization.
+ */
 interface LocaleLanguageProvider {
     fun currentLanguage(): String
-
 }
